dc0585b652a01835212da7803c642c90448db7bb
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-04-19 14:10:57 +0300

    user reducer added

---
+++ frontend/src/state/users/usersSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+interface UserState {
+    name: string,
+    email: string,
+    role: string,
+    id: string
+}
+
+const initialState: UserState = {
+    name: "Yabsera Haile",
+    email: "haileyabsera3@gmail.com",
+    role: "Student",
+    id: "ETS0660/12"
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<UserState>) => {
+      state= action.payload
+    },
+  },
+})
+
+export const { setUser } = userSlice.actions
+
+
+export default userSlice.reducer
