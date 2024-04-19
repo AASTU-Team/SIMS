90a7c1295582ee5d8e8cb3533a48e19714a5731b
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-04-19 14:11:08 +0300

    file structure improved

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,42 +1,25 @@
-"use client"
 import "./globals.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import { useEffect, useState } from "react";
-import Sidebar from "@/components/SideBar";
-import Header from "@/components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SIMS",
+  description: "Student Information Management System",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
     
-
-
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          <div className="flex h-screen overflow-hidden">
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              <Header
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              <main>
-                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                </div>
-              </main>
-            </div>
-          </div>
-        </div>
+        
+            {children}
+        
       </body>
     </html>
   );
