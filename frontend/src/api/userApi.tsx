eb91f24e6758352b546fdb2c8a6d6d39ce0945ec
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-04-29 21:35:22 +0300

    Refresh Token Integrated

--- frontend/src/api/userApi.tsx
+++ frontend/src/api/userApi.tsx
@@ -2,6 +2,8 @@ import type { AxiosResponse, AxiosError } from "axios";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { LoginForm } from "../type/user";
+import getCookie from "../hooks/getCookie";
+import removeCookie from "../hooks/removeCookie";
 
 const client = axios.create({
   baseURL: "http://localhost:5000/auth",
@@ -16,8 +18,41 @@ export const removeHeaderToken = () => {
   delete client.defaults.headers.common.Authorization;
 };
 
+export const fetchNewToken = async () => {
+  try {
+    const token: string = await client
+      .get("/refresh")
+      .then((res) => res.data.token);
+    return token;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const refreshAuth = async (failedRequest:{response:AxiosResponse} ) => {
+  const newToken = await fetchNewToken();
+
+  if (newToken) {
+    failedRequest.response.config.headers.Authorization = "Bearer " + newToken;
+    setHeaderToken(newToken);
+    return Promise.resolve(newToken);
+  } else {
+    return Promise.reject();
+  }
+};
+
 export const Login = async ({email,password}: LoginForm) =>{
   return await client.post('/login',{email,password})
+}
+
+export const Logout = async () => {
+  const refresh_token = getCookie("refresh_token") || ""
+  setHeaderToken(refresh_token)
+  const res = await client.post('/logout')
+  removeHeaderToken()
+  removeCookie("access_token")
+  removeCookie("refresh_token")
+  return res
 }
 
 export const useMe = () =>
