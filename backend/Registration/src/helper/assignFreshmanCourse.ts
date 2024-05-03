1c7a7097e4024b372ccc4352daa89fbbbe73d8d1
emran <imranhayredin89@gmail.com>
2024-05-03 16:48:14 +0300

    initalized student registration module

---
+++ backend/Registration/src/helper/assignFreshmanCourse.ts
@@ -0,0 +1,13 @@
+const Student = require("../models/student.model");
+const Department = require("../models/department.model");
+const Course = require("/../models/course.model");
+const Curriculum = require("../models/curriculum.model");
+
+
+async function getCourse(): Promise<any> {
+
+    
+
+
+
+}
