530567e63ae43a821d93803cdeff306c48905425
emran <imranhayredin89@gmail.com>
2024-05-07 19:25:35 +0300

    initalized room management

---
+++ backend/Room/src/routes/room/room.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+const Course = require("../../models/course.model");
+
+
+export const registerRoom = async (req: Request, res: Response) => {
+  const data = req.body;
+
+
+
+};
---
+++ backend/Room/src/routes/room/room.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+
+
+const Roomrouter = express.Router();
+
+//Roomrouter.post("/new", registerRoom);
+
+
+module.exports = Roomrouter;
