33bc86a76ce7b26458b69201df5a730502540255
emran <imranhayredin89@gmail.com>
2024-05-10 11:19:12 +0300

    schema fixes

--- backend/Room/src/models/Assignment.model.ts
+++ backend/Room/src/models/Assignment.model.ts
@@ -2,24 +2,27 @@ import mongoose from "mongoose";
 
 let assignmentSchema  = new mongoose.Schema({
     course_id:{
-        type:String,
-        required:false
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "Course",
     },
 
     instructor_id:{
-        type:String,
-        required:false
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "Staff",
     },
     room_number:{
-        type:String,
-        required:false
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "Room",
     },
     start_time:{
-        type:Date,
+        type:String,
         required:false
     },
     end_time:{
-        type:Date,
+        type:String,
         required:false
     },
     Lab_Lec:{
