ddb407c8718886cdd290b836d3cffac808bb62f5
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-05-13 20:09:50 +0300

    Staff Management Initialized

--- frontend/src/pages/StaffManagement/index.tsx
+++ frontend/src/pages/StaffManagement/index.tsx
@@ -1,16 +1,16 @@
-import StudentTable from "./table";
+import StaffTable from "./table";
 import { UserAddOutlined } from "@ant-design/icons";
-export default function StudentManagement() {
+export default function StaffManagement() {
   return (
     <div className="max-w-screen-2xl p-4 md:p-6 2xl:p-10">
       <div className="flex justify-between">
-        <div className="text-title-md">Student Management</div>
+        <div className="text-title-md">Staff Management</div>
         <button className="flex justify-center items-center gap-2 rounded bg-primary px-4 py-2 font-medium text-gray hover:bg-opacity-90">
           <UserAddOutlined />
-          Add Student
+          Add Staff
         </button>
       </div>
-      <StudentTable />
+      <StaffTable />
     </div>
   );
 }
