913d3b77d9992fbc235de9848680c02f7e53d242
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-05-20 19:34:17 +0300

    Bug fixes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "../hooks/protectedRoutes";
-import SignIn from "../pages/Login";
+import SignIn from "../pages/Login/index"
 import NotFoundPage from "../pages/NotFound";
 import Sidebar from "../components/SideBar";
 import Header from "../components/Header";
 import Dashboard from "../pages/Dashboard";
-import Profile from "../pages/Profile";
+import Profile from "../pages/Profile/index";
 import SetPassword from "../pages/SetPassword";
 import Loader from "../components/Loader";
 import StudentManagement from "../pages/StudentManagement";
