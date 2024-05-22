7dcba4be58d55ea4d68cad8b5549511022e83387
emran <imranhayredin89@gmail.com>
2024-05-22 12:31:59 +0300

    edited validation

--- backend/Registration/src/middlware/validateRegistration.ts
+++ backend/Registration/src/middlware/validateRegistration.ts
@@ -17,8 +17,8 @@ const validateRegistration = [
   .isDate({ format: 'YYYY-MM-DD' })
   .withMessage('Birthday must be in the format YYYY-MM-DD'),
   body('phone')
-  .matches(/^\+\d{12}$/)
-  .withMessage('Phone number must start with "+" and be followed by 12 digits'),
+  .matches(/^(\+\d{12}|\d{10})$/)
+  .withMessage('Phone number must be 10 digits or start with "+" and be followed by 12 digits'),
   body('gender')
   .isIn(['MALE', 'FEMALE'])
   .withMessage('Gender must be either "male" or "female"'),
