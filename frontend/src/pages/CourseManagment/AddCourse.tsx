be63f932006e4be397d082c2427e155448a5f8b5
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-06-04 13:16:21 +0300

    Dynamic Assessment added

--- frontend/src/pages/CourseManagment/AddCourse.tsx
+++ frontend/src/pages/CourseManagment/AddCourse.tsx
@@ -1,13 +1,16 @@
 import type { FormProps } from "antd";
 import {Form, Input, Select,notification } from "antd";
-import { CourseFields } from "../../type/course";
+import { CourseFields, Assessments } from "../../type/course";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { addCourse, getCourse } from "../../api/course";
 import { getDepartment } from "../../api/departmentApi";
 import { DepartmentFields } from "../../type/department";
+import {DeleteOutlined} from "@ant-design/icons"
+import { useState } from "react";
 
 export default function AddCourse() {
   const [form] = Form.useForm();
+  const [assessments, setAssessments] = useState<Assessments[]>([]);
   const departmentQuery = useQuery({
       queryKey: ["department"],
       queryFn: getDepartment,
@@ -40,6 +43,43 @@ export default function AddCourse() {
   ) => {
     console.log("Failed:", errorInfo);
   };
+
+  const onAddAssessment = ()=>{
+    const l = assessments.length.toString();
+    const newAsses={id:l}
+    setAssessments([... assessments, newAsses])
+  }
+   
+  const onDeleteAssessment = (id:string) =>{
+    setAssessments(assessments.filter((assessment)=>assessment.id !== id))
+    setAssessmentValue()
+  }
+
+  const onAssessmentNameChange = (name:string,id:string) => {
+    const newAssessments = assessments.map((assessment) => {
+      if (assessment.id === id) {
+        return { ...assessment, name };
+      }
+      return assessment;
+    });
+    setAssessments(newAssessments);
+  }
+  const onAssessmentValueChange = (value: number, id: string) => {
+      const newAssessments = assessments.map((assessment) => {
+        if (assessment.id === id) {
+          return { ...assessment, value };
+        }
+        return assessment;
+      });
+      setAssessments(newAssessments);
+      setAssessmentValue()
+    };
+  
+  const setAssessmentValue = () => {
+    const sum = assessments.reduce((total, assessment) => total + (assessment.value || 0), 0);
+    form.setFieldValue("assessment", sum);
+    console.log(form.getFieldValue("assessment"))
+  }
 
   const filterOption = (
     input: string,
@@ -278,6 +318,45 @@ export default function AddCourse() {
             </div>
             <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
               <Form.Item<CourseFields>
+                name="option"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input the option!",
+                  },
+                ]}
+              >
+                <div>
+                  <label
+                    className="mb-3 block text-sm font-medium text-black dark:text-white"
+                    htmlFor="option"
+                  >
+                    Option
+                  </label>
+                  <div className=" rounded-lg w-100 border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary">
+                    <Select
+                      showSearch
+                      placeholder="Select course option"
+                      optionFilterProp="children"
+                      filterOption={filterOption}
+                      onChange={(value) => {
+                        form.setFieldValue("option", value);
+                      }}
+                      options={[
+                        {
+                          value: "mandatory",
+                          label: "Mandatory Course",
+                        },
+                        {
+                          value: "elective",
+                          label: "Elective Course",
+                        },
+                      ]}
+                    />
+                  </div>
+                </div>
+              </Form.Item>
+              <Form.Item<CourseFields>
                 name="lec"
                 rules={[
                   {
@@ -321,6 +400,8 @@ export default function AddCourse() {
                   />
                 </div>
               </Form.Item>
+            </div>
+            <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
               <Form.Item<CourseFields>
                 name="tut"
                 rules={[
@@ -343,8 +424,6 @@ export default function AddCourse() {
                   />
                 </div>
               </Form.Item>
-             </div>
-             <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
               <Form.Item<CourseFields>
                 name="hs"
                 rules={[
@@ -387,6 +466,94 @@ export default function AddCourse() {
                     placeholder="Enter the description"
                     className=" rounded-lg w-100 border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
+                </div>
+              </Form.Item>
+            </div>
+            <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+              <Form.Item
+                name="assessment"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please make sure the sum of all assessments is 100!",
+                    validator: (_, value) => {
+                      if (value === 100) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        "The assessment value must be 100!"
+                      );
+                    },
+                  },
+                ]}
+              >
+                <label
+                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  htmlFor="description"
+                >
+                  Assessments
+                </label>
+                <div className="flex flex-col gap-3">
+                  { assessments &&
+                   assessments.map((assessment)=>{
+                    return (
+                      <div className="flex  gap-2 rounded-lg border border-stroke bg-transparent justify-center items-center p-3">
+                        <div>
+                          <Input
+                            placeholder="Enter the assessment name"
+                            className=" rounded-lg w-60 border border-stroke bg-transparent py-1 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            id={assessment.id}
+                            value={assessment?.name || ""}
+                            onChange={(event) => {
+                              const inputId = event.currentTarget.id;
+                              const inputValue = event.currentTarget.value;
+                              onAssessmentNameChange(inputValue, inputId);
+                            }}
+                          />
+                        </div>
+                        <div>
+                          <Input
+                            placeholder="Enter the assessment value"
+                            type="number"
+                            className=" rounded-lg w-60 border border-stroke bg-transparent py-1 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            id={assessment.id}
+                            value={assessment?.value || ""}
+                            onChange={(event) => {
+                              const inputId = event.currentTarget.id;
+                              const inputValue = Number(event.currentTarget.value); // Convert the inputValue to a number
+                              onAssessmentValueChange(inputValue, inputId);
+                            }}
+                          />
+                        </div>
+                        <div
+                          id={assessment.id}
+                          key={assessment.id}
+                          className="cursor-pointer"
+                        >
+                          <DeleteOutlined
+                            id={assessment.id}
+                            key={assessment.id}
+                            name={assessment.id}
+                            onClick={(event) => {
+                              const iconId = event.currentTarget.id;
+                              onDeleteAssessment(iconId);
+                            }}
+                            style={{ fontSize: "20px", color: "#cc0000" }}
+                          />
+                        </div>
+                      </div>
+                    );
+                   })
+                  }
+                  <button
+                    className="flex justify-center items-center gap-2 rounded-lg bg-primary px-2 py-1 font-medium text-md text-gray hover:bg-opacity-90"
+                    onClick={(e)=>{
+                      e.preventDefault()
+                      onAddAssessment()
+                    }}
+                  >
+                    Add Assessment
+                  </button>
                 </div>
               </Form.Item>
             </div>
