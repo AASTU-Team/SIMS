5d463017b8c248242a217e335683f027b3f6a9ea
bereketsintayehu <bereketsintayehu32@gmail.com>
2024-06-09 22:55:53 +0300

    assginment3

--- backend/Grading/src/helper/assignInstructor.ts
+++ backend/Grading/src/helper/assignInstructor.ts
@@ -9,24 +9,25 @@ async function assignInstructor(Stud_id: any, courses: any[]): Promise<any> {
     const success: any = []
 
     const student = await Student.findById(Stud_id)
-    if (!student) return
+    if (!student) return { error: 'Student not found'}
     const year = student.semester
     const semester = student.semester
 
     const registration = await Registration.findOne({ stud_id: Stud_id, semester: semester, year: year })
-    if (!registration) return
+    if (!registration) return { error: 'Student not registered for this semester'}
 
     const section_id = registration.section_id
 
     for (const course of courses) {
         const assignment = await Assignment.findOne({ course_id: course, section_id: section_id })
-        if (!assignment) return
+        if (!assignment) return { error: 'Assignment not found'}
         const instructor_id = assignment.instructor_id
 
         const updateGrade = await Grade.findOneAndUpdate({ student_id: Stud_id, course_id: course }, { instructor_id: instructor_id })
         if (!updateGrade) error.push(course)
         else {
             success.push(course)
+            return { success, error }
         }
     }
 }
