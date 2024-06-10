bc4d23cde3f601fc30f9ad2e4c56db8e54de74a7
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-06-11 02:05:14 +0300

    Backend bug fix

--- backend/Grading/src/helper/helper.ts
+++ backend/Grading/src/helper/helper.ts
@@ -11,6 +11,7 @@ export const assign = async () => {
             const { course_id, section_id, instructor_id, year, semester } = assignment;
 
             const numberOfStudents = await NumberOfStudent.findOne({ course_id, section_id });
+            
             if (!numberOfStudents) {
                 continue;
             }
@@ -18,11 +19,11 @@ export const assign = async () => {
             const studentIds = numberOfStudents.numberOfStudent.map((entry: any) => entry.student);
 
             const updateResult = await Grade.updateMany(
-                { student_id: { $in: studentIds }, course_id: course_id, year: year, semester: semester },
+                { student_id: { $in: studentIds }, course_id: course_id},
                 { $set: { instructor_id: instructor_id } }
             );
 
-            console.log(`Instructor ${instructor_id} updated for ${updateResult} students in course ${course_id}.`);
+            // console.log(`Instructor ${instructor_id} updated for ${updateResult} students in course ${course_id}.`);
         }
     } catch (error) {
         return
